e6254cbcf4329bc6276e0fb3bd3500117b99e03e
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2024-08-30 17:37:48 -0400

    Delete kab8 directory

--- kab8/ananya_taaruni_steps.py
+++
@@ -1,8 +0,0 @@
-
-def feet_to_steps(feet_walked):
-    steps_walked = feet_walked/2.5
-    return int(steps_walked)
-
-feet_walked = float(input('Please enter the distance walked in feet: '))
-
-print(f'{feet_to_steps(feet_walked)} steps')
